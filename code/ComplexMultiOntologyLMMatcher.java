16f3088a67f5ccd9ca9ac6bb5fdab8a275abed1b
Marta C. Silva <47527249+martacds@users.noreply.github.com>
2025-09-09 19:10:38 +0100

    Update and rename ComplexMultiOntologyLLMMatcher.java to ComplexMultiOntologyLMMatcher.java

--- code/ComplexMultiOntologyLMMatcher.java
+++ code/ComplexMultiOntologyLMMatcher.java
@@ -25,22 +25,22 @@ import liseda.matcha.settings.Settings;
 import liseda.matcha.settings.StopList;
 import liseda.matcha.similarity.Similarity;
 
-public class ComplexMultiOntologyLLMMatcher {
+public class ComplexMultiOntologyLMMatcher {
 
 	protected static final String NAME = "Complex Multi-Ontology LLM Matcher";
 	DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 
 	private HashMap<String,float[]> embeddings;
-	private HashMap<String, Set<String>> src_name2uri;
-	private HashMap<String, Set<String>> tgt_name2uri;
+	private HashMap<String, HashSet<String>> src_name2uri;
+	private HashMap<String, HashSet<String>> tgt_name2uri;
 	private HashMap<String, HashSet<String>> tgt_name2namespace = new HashMap<String, HashSet<String>>();
-	private HashMap<String, HashSet<String>> tgt_name2combos = new HashMap<String, HashSet<String>>();
+	private HashMap<HashSet<String>, HashSet<HashSet<String>>> tgt_name2combos = new HashMap<HashSet<String>, HashSet<HashSet<String>>>();
 	private int maxCardinality;
 
 	SemanticMap sm = SemanticMap.getInstance();
 	Settings settings = Settings.getInstance();
 
-	public MultiplexLLMMatcher_clean(int maxCardinality) {
+	public ComplexMultiOntologyLMMatcher(int maxCardinality) {
 		this.maxCardinality = maxCardinality;
 		StopList.init(ResourceManager.getStopSet());
 	}
@@ -50,6 +50,7 @@ public class ComplexMultiOntologyLLMMatcher {
 		long time = System.currentTimeMillis()/1000;
 
 		this.embeddings = embeddings;
+		GeometricOperations go = new GeometricOperations();
 
 		Alignment a = new Alignment(srcs, tgts);
 		src_name2uri = getNames(srcs);
@@ -60,16 +61,14 @@ public class ComplexMultiOntologyLLMMatcher {
 		embeddings.putAll(EmbeddingsParser.findMissingEmbeddings(embeddings, tgt_name2uri.keySet()));
 
 		// Finding target combinations for each source name
-		HashMap<String, String> candidate_combinations = new HashMap<String, String>();
+		HashMap<String, HashSet<String>> candidate_combinations = new HashMap<String, HashSet<String>>();
 		System.out.println("Finding target word combinations for each source");
 		long combos_time = System.currentTimeMillis()/1000;
 		for(String src_name : src_name2uri.keySet()) {
-			String best_combination = createBestCombination(src_name);
-			if(!best_combination.isBlank()) {
-				// check URIs for duplicates
-				HashSet<String> uri_combinations = new HashSet<String>();
-				uri_combinations = CMOMextras.findURIcombinations(tgt_name2uri, best_combination);
-
+			HashSet<String> best_combination = go.findBestTargetNameCombination(embeddings, embeddings.get(src_name), tgt_name2uri.keySet());
+			if(!best_combination.isEmpty()) {
+				// find URI combinations for name combination
+				HashSet<HashSet<String>> uri_combinations = go.convertNameSetstoIRIsets(best_combination, tgt_name2uri);
 				if(!uri_combinations.isEmpty()) {
 					candidate_combinations.put(src_name, best_combination);
 					if(!tgt_name2combos.containsKey(best_combination))
@@ -84,163 +83,38 @@ public class ComplexMultiOntologyLLMMatcher {
 		// alignment with all possible combinations
 		for(String src_name : candidate_combinations.keySet()) {
 			for(String src_uri : src_name2uri.get(src_name)) {				
-				String tgt_name_combination = candidate_combinations.get(src_name);
-
-				for(String tgt_uri_combination : tgt_name2combos.get(tgt_name_combination)) {
-
-					Double sim = calculateSimilarity(src_name, tgt_name_combination);
+				HashSet<String> tgt_name_combination = candidate_combinations.get(src_name);
+				for(HashSet<String> tgt_uri_combination : tgt_name2combos.get(tgt_name_combination)) {
+					Double sim = go.calculateSimilarity(embeddings, src_name, tgt_name_combination);
 					if(sim >= thresh) {
-						Mapping m = new Mapping(src_uri, createClassIntersection(tgt_uri_combination), sim, MappingRelation.EQUIVALENCE);
-						a.add(m);
+						a.add(new Mapping(src_uri, tgt_uri_combination, sim, MappingRelation.EQUIVALENCE));
 					}
 				}
 			}
 		}
 		a.sortDescending();
-
 		time = System.currentTimeMillis()/1000 - time;
 		System.out.println(dtf.format(LocalDateTime.now()) + " | " + NAME + " finished in " + time + " seconds");
-
 		return a;
 	}
-	
-	private Double calculateSimilarity(String src_name, String tgt_name_combination) {
 
-		float[] src_emb = embeddings.get(src_name);
-
-		String[] name_array = tgt_name_combination.split(" # "); // split target combination into names
-		float[] sum = embeddings.get(name_array[0]); // initialise product w/ first embedding
-
-		for(int i = 1; i < name_array.length; i++) { // iterate through all targets in combination
-			float[] emb = embeddings.get(name_array[i]);
-			float[] calc = new float[emb.length];
-
-			for(int j = 0; j < sum.length; j++) { // iterate through positions in the vector
-				calc[j] = sum[j]+emb[j];
-			}
-			sum = calc; // update combined embedding
-		}
-
-		return Similarity.cosineSimilarity(src_emb, sum);
-	}
-	
-	private String createClassIntersection(String tgt_uri_combination) {
-		String[] split = tgt_uri_combination.split(" # ");
-		List<ClassExpression> listCE = new ArrayList<ClassExpression>();
-
-		for(String uri : split) {
-			listCE.add(new SimpleClass(uri));
-		}
-		ClassIntersection ci = new ClassIntersection(listCE);
-		sm.addExpression(ci);
-		return ci.toString();
-	}
-	
-	private String createBestCombination(String src_name) {
-		HashSet<String> best_combination = new HashSet<String>();
-		float[] srcEmb = embeddings.get(src_name);
-
-		// 1
-		String first_name = selectBestTarget(srcEmb);
-		best_combination.add(first_name);
-		float[] newEmb = subtractEmbedding(srcEmb, first_name);
-
-		// 2
-		String new_name = selectBestTarget(newEmb);
-		if(!new_name.isEmpty()) {
-			best_combination.add(new_name);
-			newEmb = subtractEmbedding(newEmb, new_name);
-		}
-
-		// 3
-		if(maxCardinality > 2) {
-			new_name = selectBestTarget(newEmb);
-			if(!new_name.isEmpty()) {
-				best_combination.add(new_name);
-				newEmb = subtractEmbedding(newEmb, new_name);
-
-				// 4
-				if(maxCardinality > 3) {
-					new_name = selectBestTarget(newEmb);
-					if(!new_name.isEmpty()) {
-						best_combination.add(new_name);
-						newEmb = subtractEmbedding(newEmb, new_name);
-
-						// 5
-						if(maxCardinality > 4) {
-							new_name = selectBestTarget(newEmb);
-							if(!new_name.isEmpty()) {
-								best_combination.add(new_name);
-								newEmb = subtractEmbedding(newEmb, new_name);
-
-								// 6
-								if(maxCardinality > 5) {
-									new_name = selectBestTarget(newEmb);
-									if(!new_name.isEmpty()) {
-										best_combination.add(new_name);
-										newEmb = subtractEmbedding(newEmb, new_name);
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-		if(best_combination.isEmpty())
-			return "";
-		else if(best_combination.size() > maxCardinality)
-			return "";
-		return String.join(" # ", best_combination);
-	}
-	
-	private String selectBestTarget(float[] srcEmb) {
-
-		double best_sim = 0.0;
-		String best_name = "";
-
-		for(String tgt_name : tgt_name2uri.keySet()) {
-			float[] tgtEmb = embeddings.get(tgt_name);
-			Double sim = Similarity.cosineSimilarity(srcEmb, tgtEmb);
-
-			if(sim > best_sim) {				
-				best_sim = sim;
-				best_name = tgt_name;
-			}
-		}
-		if(best_sim < 0.2) {
-			return "";
-		}
-		return best_name;
-	}
-	
-	private float[] subtractEmbedding(float[] prevEmb, String tgt_name) {
-		float[] newEmb = new float[prevEmb.length];
-		float[] tgtEmb = embeddings.get(tgt_name);
-
-		for(int j = 0; j < newEmb.length; j++) {
-			newEmb[j] = prevEmb[j]-tgtEmb[j];
-		}
-		return newEmb;
-	}
-
-	private HashMap<String, Set<String>> getNames(List<Ontology> ontologies) {
-		HashMap<String, Set<String>> name2uri = new HashMap<String, Set<String>>();
+	private HashMap<String, HashSet<String>> getNames(List<Ontology> ontologies) {
+		HashMap<String, HashSet<String>> name2uri = new HashMap<String, HashSet<String>>();
 		for(Ontology o : ontologies) {
 			Lexicon lex = o.getLexicon(EntityType.CLASS);
-			String namespace = CMOMextras.getNamespace(o.getURI(), true);
+			String namespace = AuxiliaryMethods.getNamespace(o.getURI(), true);
 
 			for(String uri : o.getEntities(EntityType.CLASS)) {
 				if(uri.toLowerCase().contains(namespace)) { // only internal classes
 					for(String name : lex.getNames(uri)) {
 						if(!name.contains("/") && name.length() > 2 && !name.contains("[") && !name.contains("{") && !name.contains(":") && !name.contains("&") && !name.contains("=") && !name.equals("[0-9]+")) {
 							if(name2uri.containsKey(name)) {
-								Set<String> set = name2uri.get(name);
+								HashSet<String> set = name2uri.get(name);
 								set.add(uri);
 								name2uri.replace(name, set);
 							}
 							else {
-								Set<String> set = new HashSet<String>();
+								HashSet<String> set = new HashSet<String>();
 								set.add(uri);
 								name2uri.put(name, set);
 							}
@@ -253,11 +127,11 @@ public class ComplexMultiOntologyLLMMatcher {
 		return name2uri;
 	}
 
-	private HashMap<String, Set<String>> getNamesAndNamespaces(List<Ontology> ontologies) {
-		HashMap<String, Set<String>> name2uri = new HashMap<String, Set<String>>();
+	private HashMap<String, HashSet<String>> getNamesAndNamespaces(List<Ontology> ontologies) {
+		HashMap<String, HashSet<String>> name2uri = new HashMap<String, HashSet<String>>();
 		for(Ontology o : ontologies) {
 			Lexicon lex = o.getLexicon(EntityType.CLASS);
-			String namespace = CMOMextras.getNamespace(o.getURI(), true);
+			String namespace = AuxiliaryMethods.getNamespace(o.getURI(), true);
 
 			for(String uri : o.getEntities(EntityType.CLASS)) {
 				if(uri.toLowerCase().contains(namespace)) { // only internal classes
@@ -265,12 +139,12 @@ public class ComplexMultiOntologyLLMMatcher {
 						if(!name.contains("/") && name.length() > 2 && !name.contains("[") && !name.contains("{") && !name.contains(":") && !name.contains("&") && !name.contains("=") && !name.equals("[0-9]+")) {
 							// save name and uris
 							if(name2uri.containsKey(name)) {
-								Set<String> set = name2uri.get(name);
+								HashSet<String> set = name2uri.get(name);
 								set.add(uri);
 								name2uri.replace(name, set);
 							}
 							else {
-								Set<String> set = new HashSet<String>();
+								HashSet<String> set = new HashSet<String>();
 								set.add(uri);
 								name2uri.put(name, set);
 							}
@@ -293,5 +167,6 @@ public class ComplexMultiOntologyLLMMatcher {
 		}
 		System.out.println("Found " + name2uri.size() + " names");
 		return name2uri;
-  }
+	}
+
 }
